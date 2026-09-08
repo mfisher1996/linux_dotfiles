6de6ab74c8f2ee5c4d3fa816492acf01c6190ca8
Mason Fisher <masonfisherb@gmail.com>
2026-09-08 15:10:36 -0500

    user now flake argument

--- flake.nix
+++ flake.nix
@@ -1,26 +1,31 @@
-# masonf is my user on work computer
 {
-  description = "Modular Home Manager Configuration Flake";
-
-  inputs = {
-    # this might need to be set to something more stable later if I run into issues.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    description = "Modular Home Manager Configuration Flake";
+    inputs = {
+        # this might need to be set to something more stable later if I run into issues.
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
-  outputs = { nixpkgs, home-manager, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      homeConfigurations."masonf" = home-manager.lib.homeManagerConfiguration {
+    outputs = { nixpkgs, home-manager, ... }:
+        let
+        system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+    mkHomeConfig = username: home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        modules = [ ./home.nix ];
-      };
+        modules = [
+            ./home.nix
+            {home.username = username;
+                home.homeDirectory = "/home/${username}";
+            }
+        ];
+    };
+    in {
+        homeConfigurations = {
+            "masonf" = mkHomeConfig "masonf";
+            "mason" = mkHomeConfig "mason";
+        };
     };
 }
--- home.nix
+++ home.nix
@@ -1,7 +1,5 @@
 { config, pkgs, ... }:
 {
-    home.username = "masonf";
-    home.homeDirectory = "/home/masonf";
     home.stateVersion = "24.05";
 
     programs.home-manager.enable = true;

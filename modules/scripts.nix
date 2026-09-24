c85d8b30c9106763d67e5556a6674a2539d32e8e
Mason Fisher <masonfisherb@gmail.com>
2026-09-24 14:06:01 -0500

    scripts

--- modules/scripts.nix
+++ modules/scripts.nix
@@ -1,4 +1,8 @@
 { config, pkgs, ... }:
 {
-    home.file.".local/scripts/new_project".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/scripts/new_project";
+    #home.file.".local/scripts/new_project".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/scripts/new_project";
+    home.file.".local/scripts/new_project" = {
+        source = "${config.home.homeDirectory}/.dotfiles/scripts/new_project";
+        executable =true;
+    };
 }

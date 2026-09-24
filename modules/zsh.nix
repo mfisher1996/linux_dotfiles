37582578a18d146c713c2051e2375a3df0348b36
Mason Fisher <masonfisherb@gmail.com>
2026-09-24 15:43:54 -0500

    using home-manager zsh config now

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,17 +1,36 @@
-{ config, pkgs, ... }:
+{ config, pkgs, ... }: # Fix 1: Removed 'programs' from arguments
 
 {
-  home.packages = with pkgs; [
-    zsh
-    zsh-vi-mode
-    zsh-history-substring-search
-    fzf
-    ripgrep
-    fd
-  ];
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        
+        initExtraBeforeCompInit = ''
+          fpath=(${config.home.homeDirectory}/.dotfiles/zsh_comps $fpath)
+        '';
 
-  home.file.".zshrc".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/zsh/.zshrc";
-  home.file.".zsh_vim".source =
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/config/zsh/.zsh_vim";
+        # Fix 2: Put your raw, static ~/.zshrc content right here
+        # Instead of a symlink, read the file directly into Home Manager
+        initExtra = ''
+          source ${config.home.homeDirectory}/.dotfiles/config/zsh/.zsh_vim
+          
+          # Read your original .zshrc text straight from the dotfiles
+          ${builtins.readFile "${config.home.homeDirectory}/.dotfiles/config/zsh/.zshrc"}
+          ${builtins.readFile "${config.home.homeDirectory}/.dotfiles/config/zsh/.zsh_vim"}
+
+        '';
+    };
+
+    home.packages = with pkgs; [
+        zsh-vi-mode
+        zsh-history-substring-search
+        fzf
+        ripgrep
+        fd
+    ];
+
+    # Safe to keep if it doesn't conflict with main shell control
+    #home.file.".zsh_vim".source =
+      #config.lib.file.mkOutOfStoreSymlink;
 }
+
